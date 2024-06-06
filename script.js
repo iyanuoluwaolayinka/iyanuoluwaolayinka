6f874d1fc65bc780675d103180905498e52bf05b
iyanuoluwaolayinka <iyanuoluwaolayinka123@gmail.com>
2024-06-06 13:46:48 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// // Javascript for navbar
+// window.addEventListener("scroll", function () {
+//     var header = this.document.queryselector("header");
+//     header.classlist.toggle("sticky", window.scrollY > 0);
+// });
+
+// //navigation sideber Menu 
+// var menu = document.querySelector(".menu");
+// var menuBtn = document.querySelector(".menu-btn");
+// var closeBtn = document.querySelector(".close-btn");
+
+// menuBtn.addEventListener("click", () => {
+//     menu.classList.add("active");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//     menu.classList.remove("active");
+// });
+
+// // Swiper
+// const Swiper = new Swiper(".swiper", {
+//     slidesPerView: 3,
+//     direction: "horizontal",
+//     lazyLoading: true,
+//     spaceBetween: 7,
+//     grabCursor:false,
+//     CentereSlides: true,
+//     loop:true,
+//     Keyboard: {
+//         enabled: true,
+//     }
+
+//     // Responsive
+
+// login
+function login(){
+    window.location.href = 'cotact'
+}
